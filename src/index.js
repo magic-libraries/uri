d706ae6b9a743450107bb423518c0db3018cabc3
Jascha <jascha@jaeh.at>
2019-05-03 04:57:45 +0200

    merge src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 module.exports = {
-  encode: require('./encode'),
-  decode: require('./decode'),
+  encode: (props = {}) => {
+    const { host = '', params = {} } = props
+
+    const query = Object.entries(params)
+      .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+      .join('&')
+
+    if (!query) {
+      return host
+    }
+
+    return [host, query].join('?')
+  },
+
+  decode: (uri = '') => {
+    if (!uri) {
+      return {}
+    }
+
+    const [host, p] = uri.split('?')
+
+    const res = { host }
+    if (p) {
+      p.split('&').map(param => {
+        res.params = res.params || {}
+        const [name, val] = param.split('=')
+        res.params[name] = val
+      })
+
+    }
+    return res
+  },
 }
